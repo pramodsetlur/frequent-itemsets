3413d4a85bb95eff06f4e737eef6643bef3a2eb0
Pramod Setlur <pramod.setlur@ticketmaster.com>
2015-09-28 09:51:53 -0700

    attempting to print pairs.

--- pramod_setlur_pcy.py
+++ pramod_setlur_pcy.py
@@ -1,11 +1,11 @@
 import sys
+import itertools
 
 singleton_sets = {}
+pairs_sets = {}
 
-
-def compute_singleton_set(transaction):
-    transaction_items = transaction.rstrip('\n')
-    transaction_items = transaction_items.split(',')
+def compute_singleton_set(list_transaction):
+    transaction_items = list_transaction.split(',')
 
     for transaction in transaction_items:
         singleton_sets.setdefault(transaction, 1)
@@ -13,9 +13,22 @@ def compute_singleton_set(transaction):
         count = count + 1
         singleton_sets[transaction] = count
 
+def compute_pairs_sets(transaction):
+    transaction_items = transaction.rstrip('\n')
+    transaction_items = transaction.strip(',')
+
+    transaction_combination_two = itertools.combinations(transaction_items, 2)
+    transaction_pairs = list(transaction_combination_two)
+
+    #print transaction_pairs
+
+
 def compute_frequent_sets_pcy(input_file, support, bucket_size):
     for transaction in open(input_file):
-        compute_singleton_set(transaction)
+        transaction.rstrip('\n')
+        list_transaction = list(transaction)
+        compute_singleton_set(list_transaction)
+        compute_pairs_sets(transaction)
     print singleton_sets
 
 if __name__ == '__main__':
