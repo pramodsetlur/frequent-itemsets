8e0919e868cbd9cfad068c0a2b3a9a1492ff3b7a
Pramod Setlur <pramod.setlur@ticketmaster.com>
2015-10-02 03:04:23 -0700

    Singleton frequent item sets getting generated

--- pramod_setlur_pcy.py
+++ pramod_setlur_pcy.py
@@ -1,35 +1,65 @@
+'''
+Algorithm for PCY
+_________________
+
+1. Initial setup
+    a. Generating the Frequent Item List of size 1 - FIL
+
+k = 2
+2. Hashing
+    a. For all baskets:
+        Read single basket
+        generate all subsets of size k
+        Hash them
+    b. Create a bit map of the hash above
+
+3. Generating the candiadate set
+    a. For all baskets:
+        Read basket
+        Generate all subsets of size k
+        For each item in the above subset:
+            Generate a subset of k-1 size
+            For each item in the subset:
+                See if it is present in the FIL
+            Check if item hashes to a frequent bucket
+            Add it into the Candidate item set
+
+4. Generating the frequent item set
+    a. For all baskets:
+        Generate subset of size k
+        For each item in the subset:
+            Check if it is present in the candidate item set
+            Add it to the Candidate Dictionary with incrementing the count
+    c. Clear FIL
+    d. Iterate through the Candidate dictionary and check if the count is more than the support. Add it to FIL
+    e. k++
+'''
+
+
+
 import sys
 import itertools
 
-singleton_sets = {}
-pairs_sets = {}
-'''
-def compute_singleton_set(list_transaction):
-    transaction_items = list_transaction.split(',')
+frequent_item_list = {}
+
+#Function to generate singleton frequent item set
+def compute_singleton_set(transaction):
+    transaction_items = transaction.strip().split(',')
 
     for transaction in transaction_items:
-        singleton_sets.setdefault(transaction, 1)
-        count = singleton_sets.get(transaction)
+        frequent_item_list.setdefault(transaction, 1)
+        count = frequent_item_list.get(transaction)
         count = count + 1
-        singleton_sets[transaction] = count
-
-def compute_pairs_sets(transaction):
-    transaction_items = transaction.rstrip('\n')
-    transaction_items = transaction.split(',')
-
-    transaction_combination_two = itertools.combinations(transaction_items, 2)
-    transaction_pairs = list(transaction_combination_two)
-
-    #print transaction_pairs
-'''
+        frequent_item_list[transaction] = count
 
 def compute_frequent_sets_pcy(input_file, support, bucket_size):
+    #Converting command line params to integers from string
+    support = int(support)
+    bucket_size = int(bucket_size)
+
     for transaction in open(input_file):
-        transaction.rstrip('\n')
-        list_transaction = list(transaction)
-        compute_singleton_set(list_transaction)
-        compute_pairs_sets(list_transaction)
-    print singleton_sets
+        compute_singleton_set(transaction)
+    print frequent_item_list
 
 if __name__ == '__main__':
     if 4 != len(sys.argv):
