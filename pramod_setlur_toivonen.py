0131061b063dcfaec4eb262a888a1bcbbea1ec0b
Pramod Setlur <pramod.setlur@ticketmaster.com>
2015-10-10 16:38:40 -0700

    Attempting to generate NBL

--- pramod_setlur_toivonen.py
+++ pramod_setlur_toivonen.py
@@ -1,7 +1,17 @@
 '''
 Algorithm for Toivonen
 ______________________
+Pass1
+1. Generate Random Sample data
+2. Generate Candidate Frequent Itemsets, Ck
+3. Generate Frequent Itemsets, Lk (using scaled support)
+4. Generate N.B.L
 
+Pass2:
+1. Generate count for whole file for the ones present in Lk and NBL
+2. Generate frequent itemsets (using actual support)
+2. If any NBL is present in the above list, resample and start algorithm again wth k = 1
+    else increment k
 '''
 
 import sys
@@ -34,7 +44,7 @@ def get_random_line_numbers(number_of_lines):
     random_line_numbers = random.sample(range(1, number_of_lines), sample_lines_required)
     return random_line_numbers
 
-def generate_random_sample(input_file, sample_percentage):
+def generate_random_sample(input_file):
     random_sample = []
     number_of_lines = get_total_number_of_lines(input_file)
     random_line_numbers = get_random_line_numbers(number_of_lines)
@@ -45,38 +55,84 @@ def generate_random_sample(input_file, sample_percentage):
             list_transaction = transaction.strip().split(',')
             if line_number in random_line_numbers:
                 random_sample.append(list_transaction)
+            line_number += 1
     file.close()
     return random_sample
 
+def generate_singleton(list):
+    singleton = []
+    for each_transaction in list:
+        for each_item in each_transaction:
+            if each_item not in singleton:
+                singleton.append(each_item)
+    singleton.sort()
+    return singleton
 
-def count_item_sets(random_sample_data, k):
-    item_set_count = {}
-    for each_random_transaction in random_sample_data:
-        subset_k_list = list(itertools.combinations(each_random_transaction, k))
-        for each_transaction in subset_k_list:
-            if 1 == len(each_transaction):
-                each_transaction = each_transaction[0]
+def generate_combinations_k_random_data(random_sample_data, k):
+    combination_random_list = []
+    for each_transaction in random_sample_data:
+        each_transaction_combinations = itertools.combinations(each_transaction, k)
+        for each_combination in each_transaction_combinations:
+            if each_combination not in combination_random_list:
+                combination_random_list.append(each_combination)
 
-            item_set_count.setdefault(each_transaction, 0)
-            count = item_set_count.get(each_transaction)
+    combination_random_list.sort()
+    return combination_random_list
+
+
+def generate_ck_dictionary(random_sample_data, lk_1_list, k):
+    ck_dictionary = {}
+    if 1 == k:
+        for each_transaction in random_sample_data:
+            for each_item in each_transaction:
+                ck_dictionary.setdefault(each_item, 0)
+                count = ck_dictionary.get(each_item)
+                count += 1
+                ck_dictionary[each_item] = count
+
+    if 2 == k:
+        combinations_random_list = generate_combinations_k_random_data(random_sample_data, k)
+        for each_transaction in combinations_random_list:
+            ck_dictionary.setdefault(each_transaction, 0)
+            count = ck_dictionary.get(each_transaction)
             count += 1
-            item_set_count[each_transaction] = count
-    return item_set_count
+            ck_dictionary[each_transaction] = count
+
+    if k >= 3:
+        combinations_random_list = generate_ck_dictionary(random_sample_data, k)
+        for each_combination in combinations_random_list:
+            subset_k_1 = itertools.combinations(each_combination, k-1)
+            flag = 1
+            for each_subset in subset_k_1:
+                if each_subset not in lk_1_list:
+                    flag = 0
+            if 1 == flag:
+                ck_dictionary.setdefault(each_combination, 0)
+                count = ck_dictionary.get(each_combination)
+                count += 1
+                ck_dictionary[each_combination] = count
+
+    return ck_dictionary
+
+def generate_lk_list(ck_dictionary):
+    lk_list = []
+    for item, count in ck_dictionary.iteritems():
+        if count > scaled_support:
+            if item not in lk_list:
+                lk_list.append(item)
+    lk_list.sort()
+    return lk_list
 
 
-def generate_candidate_item_sets(random_sample_data, k):
-    item_set_count = count_item_sets(random_sample_data, k)
-    candidate_item_set = []
-    for each_item, count in item_set_count.iteritems():
-        if count >= scaled_support:
-            if each_item not in candidate_item_set:
-                candidate_item_set.append(each_item)
+def generate_nbl_list(ck_dictionary, lk_list, k):
+    if 1 == k:
 
-    return  candidate_item_set
 
-def generate_frequent_item_set_apriori(random_sample_data, k):
-    candidate_item_set = generate_candidate_item_sets(random_sample_data, k)
-    print candidate_item_set
+    for each_item in lk_list:
+
+
+def generate_ck_list_from_dictionary(ck_dictionary):
+
 
 if __name__ == '__main__':
     if 3 != len(sys.argv):
@@ -88,5 +144,11 @@ if __name__ == '__main__':
         update_scaled_support(support)
 
         k = 1
-        random_sample_data = generate_random_sample(input_file, sample_percentage)
-        frequent_item_list = generate_frequent_item_set_apriori(random_sample_data, k)
+        random_sample_data = generate_random_sample(input_file)
+        if 1 == k:
+            lk_1_list = random_sample_data
+        ck_dictionary = generate_ck_dictionary(random_sample_data, lk_1_list, k)
+        lk_list = generate_lk_list(ck_dictionary)
+        ck_list = generate_ck_list_from_dictionary(ck_dictionary)
+        #print lk_list
+        nbl_list = generate_nbl_list(ck_dictionary, lk_list, k)
